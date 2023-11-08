7cd333208fafe4f80ffd7ea7998dac94f43665c5
Alok Raj <137365623+alokrajcse@users.noreply.github.com>
2023-11-08 23:17:18 +0530

    Create MyName.java

---
+++ MyName.java
@@ -0,0 +1,14 @@
+/*
+LAB QUESTION2
+*/
+
+public class PrintName {
+
+    public static void main(String[] args) {
+        String name = "Alok";
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ". " + name);
+        }
+    }
+}
